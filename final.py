aff1bc6ed4cafee2e51c532814a10475c9d9d4ab
jlbriney <jordanbriney@Jacks-MacBook-Pro.local>
2020-04-22 23:20:06 -0400

    so far

--- final.py
+++ final.py
@@ -1,9 +1,31 @@
 import csv
 import sqlite3
 import os
+import requests
+import io
+import pandas as pd
 
-#create three functions 
- 
+#read into github website to get raw file
+url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv'
+data = requests.get(url).content
+#convert bytes to string format
+corona_info = str(data, 'utf-8')
+
+corona = corona_info.split(' ')
+
+
+
+
+#write csv file from github data
+with open('corona.csv', 'w', newline = '') as f:
+    write = csv.writer(f)
+    for info in corona:
+        info = info.split('\n')
+        for num in info:
+            num = num.split(',')
+            write.writerow([num[0], num[1], num[2]])
+
+#read through csv file
 def csv_reader(filename):
     root_path = os.path.dirname(os.path.abspath(__file__))
     filename = os.path.join(root_path, filename)
@@ -15,7 +37,7 @@ def csv_reader(filename):
         file_data[i] = file_data[i].replace('\n', '')
     return file_data[1:]
 
-us_corona_info = csv_reader('us.csv')
+us_corona_info = csv_reader('corona.csv')
 
 #read through csv file and return a list of all of the dates 
 def get_date_data(info):
@@ -54,14 +76,105 @@ no_of_deaths = get_deaths_data(us_corona_info)
 def open_db():
     conn = sqlite3.connect('/Users/jordanbriney/Desktop/Coronavirus.db')
     cur = conn.cursor()
-    cur.execute("DROP TABLE IF EXISTS USCases")
+    cur.execute('DROP TABLE IF EXISTS USCases')
     cur.execute("CREATE TABLE IF NOT EXISTS USCases(date TEXT, cases INTEGER, deaths INTEGER)") 
-    pass
-
-#def write_db(date_data, cases_data, deaths_data): 
- #   try:
-    #    conn = sqlite3.connect('/Users/jordanbriney/Desktop/Coronavirus.db')
-     #   cur = conn.cursor()
     
+#create database table from csv file info
+def write_db(date_info, no_of_cases, no_of_deaths): 
+    try:
+        conn = sqlite3.connect('/Users/jordanbriney/Desktop/Coronavirus.db')
+        cur = conn.cursor()
+        for x in range(20):
+            date = date_info[x]
+            cases = no_of_cases[x]
+            deaths = no_of_deaths[x]
+            cur.execute("INSERT INTO USCases (date, cases, deaths) VALUES (?,?,?)", (date, cases, deaths))
+            conn.commit()
+        print('Success')
+        cur.close()
+    except:
+        print("Data inputed in table but not enough data for 20 new rows")
+
+#limiting to 20 uploads at a time
+open_db()
+write_db(get_date_data(us_corona_info), get_cases_data(us_corona_info), get_deaths_data(us_corona_info))
+write_db(get_date_data(us_corona_info)[20:], get_cases_data(us_corona_info)[20:], get_deaths_data(us_corona_info)[20:])
+write_db(get_date_data(us_corona_info)[40:], get_cases_data(us_corona_info)[40:], get_deaths_data(us_corona_info)[40:])
+write_db(get_date_data(us_corona_info)[60:], get_cases_data(us_corona_info)[60:], get_deaths_data(us_corona_info)[60:])
+write_db(get_date_data(us_corona_info)[80:], get_cases_data(us_corona_info)[80:], get_deaths_data(us_corona_info)[80:])
+
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+#read through corona.csv to make each column into a list
+with open('corona.csv', 'r') as csv_file:
+    csv_read = csv.reader(csv_file, delimiter = ',')
+    date = []
+    num_cases = []
+    num_deaths = []
+    for lines in csv_read:
+        date.append(lines[0])
+        num_cases.append(lines[1])
+        num_deaths.append(lines[2])
+    date_list = date[1:]
+    cases_list = num_cases[1:]
+    cases_list = [int(i) for i in cases_list]
+    deaths_list = num_deaths[1:]
+    deaths_list = [int(i) for i in deaths_list]
+    
+    y = date_list
+    d1 = cases_list
+    d2 = deaths_list
+
+    fig, ax = plt.subplots()
+    ax.plot(y, d1, 'b-', label = "Number of Cases")
+    ax.plot(y, d2, 'g-', label = "Number of Deaths")
+    ax.legend()
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Number of People')
+    ax.set_title('COVID-19 in the USA')
+    plt.xticks(rotation = 'vertical', fontsize = 4)
+    ax.grid()
+    # save the line graph
+    fig.savefig("test.png")
+
+    # show the line graph
+    plt.show()
 
         
+#calculation
+def get_db_info():
+    conn = sqlite3.connect('/Users/jordanbriney/Desktop/Coronavirus.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM USCases")
+    result = cur.fetchall()
+    return result
+
+lst_uscases = get_db_info()
+
+def make_cases_lst(lst1):
+    cases = []
+    for info in lst1:
+        cases.append(info[1])
+    return cases
+
+cases1 = make_cases_lst(lst_uscases)
+
+def make_deaths_lst(lst1):
+    deaths = []
+    for info in lst1:
+        deaths.append(info[2])
+    return deaths
+
+deaths1 = make_deaths_lst(lst_uscases)
+
+def num_deaths_per_cases(info1, info2):
+    average = [int(i)/int(j) for i, j in zip(info1, info2)]
+    return average
+
+deaths_per_cases = num_deaths_per_cases(deaths1, cases1)
+print(deaths_per_cases)
+
+
+
